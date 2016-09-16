54660f1b7badcd09fde084495c07dc5ac46e888c
Clifton Regis <github email address>
2016-09-16 11:32:58 -0400

    Create student spec, @hoslersk

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,6 @@
 class Student < ApplicationRecord
   has_many :courses
   has_many :teachers, through: :courses
-
   validates_inclusion_of :gender, :in => %w( m f M F)
-
+  validates :first_name, :last_name, :age, presence: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ApplicationRecord
   has_many :courses
   has_many :students, through: :courses
-
   validates_inclusion_of :gender, :in => %w( m f M F)
+  validates :first_name, :last_name, :age, presence: true
 
 end
